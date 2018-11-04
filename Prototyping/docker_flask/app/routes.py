0c5438c0d5c1fdc6addd24d09555457b07709f13
dsegundo2 <diegosegundo@umail.ucsb.edu>
2018-11-04 14:56:33 -0800

    updated so that /search will return a json object

--- Prototyping/docker_flask/app/routes.py
+++ Prototyping/docker_flask/app/routes.py
@@ -1,11 +1,32 @@
 from app import app
 import json
+from flask import Flask, jsonify
+
+messages = [
+    {
+        'id': 1,
+        'title': 'SearchResult1',
+        'description': 'description for Title1',
+        'done': False
+    },
+    {
+        'id': 1,
+        'title': 'SearchResult2',
+        'description': 'description for Title2',
+        'done': False
+    }
+]
+
+home = {'home' : 'this is the json returned to the homepage'}
+
+
+@app.route('/search', methods=['GET'])
+def get_tasks():
+    return jsonify({'messages': messages})
 
 
 @app.route('/')
 @app.route('/index')
 def index():
-    j = {'message' : 'Hello World'}
-    return j
-
-    # return "Hello, World!"
+    return "This is the homepage!"
+    # return jsonify(home)
