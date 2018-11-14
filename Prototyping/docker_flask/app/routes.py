a5b6619d3d59aa2ee2b8cd5e707de9c7df1f147d
zspar <grahammfoster96@gmail.com>
2018-11-13 19:55:52 -0800

    stuff

--- Prototyping/docker_flask/app/routes.py
+++ Prototyping/docker_flask/app/routes.py
@@ -1,5 +1,8 @@
 from app import app
 import json
+import urllib.request
+import urllib
+import requests
 from flask import Flask, jsonify
 
 messages = [
@@ -20,10 +23,18 @@ messages = [
 home = {'home' : 'this is the json returned to the homepage'}
 
 
-@app.route('/search', methods=['GET'])
-def get_tasks():
-    return jsonify({'messages': messages})
+def get_pwned(email):
+    pwned_url = "https://haveibeenpwned.com/api/v2/breachedaccount/" + email +"?includeUnverified=true"
+    paste_url = "https://haveibeenpwned.com/api/v2/pasteaccount/" + email +"?includeUnverified=true"
+    request  = requests.get(pwned_url)
+    return (request.text)
 
+
+
+@app.route('/search/<string:email>', methods=['GET'])
+def get_tasks(email):
+    #return jsonify({'messages': messages})
+    return (get_pwned(email))
 
 @app.route('/')
 @app.route('/index')
