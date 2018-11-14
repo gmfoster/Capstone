30074be309b77283ba594ffe780ac9398db2467d
Blake Johnson <brjohnson61@gmail.com>
2018-11-13 20:38:12 -0800

    reverted image tag

--- Prototyping/docker_flask/rebuild.sh
+++ Prototyping/docker_flask/rebuild.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 docker kill novasight
-docker rmi -f latest
+docker rmi -f novasight:latest
 
 docker build -t novasight:latest .
 docker run --name novasight -d -p 8000:5000 --rm novasight:latest
