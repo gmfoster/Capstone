cb259396637e833dac35db9e4ae4dec6d9e2e621
gmfoster <grahammfoster96@gmail.com>
2018-11-15 16:05:34 -0800

    working search pwned

--- Prototyping/docker_flask/app/pwned.py
+++ Prototyping/docker_flask/app/pwned.py
@@ -4,7 +4,7 @@ import requests
 import json
 
 class Pwned_Module():
-    def __init__(self, url_1 ="https://haveibeenpwned.com/api/v2/breacdedaccount/", url_2 ="?includeUnverified=true"):
+    def __init__(self, url_1 ="https://haveibeenpwned.com/api/v2/breachedaccount/", url_2 ="?includeUnverified=true"):
         self.url_1 = url_1
         self.url_2 = url_2
 
