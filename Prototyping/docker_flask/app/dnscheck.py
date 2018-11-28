d07e11c7d7291645139d5d2ca5b93d2eba88ce30
gmfoster <grahammfoster96@gmail.com>
2018-11-28 13:58:46 -0800

    test of vs code

--- Prototyping/docker_flask/app/dnscheck.py
+++ Prototyping/docker_flask/app/dnscheck.py
@@ -17,8 +17,6 @@ class DNS_Check():
         self.api_key = api_key
         self.ping = self.session.get(base_url + "ping")
         
-    this is a test
-        
     def test_connect(self):
         endpoint = '{}/ping/'.format(self.base_url)
         r = self.session.get(endpoint)
