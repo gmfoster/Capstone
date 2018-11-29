bb4e5d828412b3edb6546caf17aa58a8d91e7873
omerco1 <31459523+omerco1@users.noreply.github.com>
2018-11-28 21:42:53 -0800

    front end directory clean up

---
+++ novasight_front/build/precache-manifest.b31df4b5abdaa86ebaf2debe717d4ba4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "5c286499f8e04e62f1bd",
+    "url": "/static/js/main.5c286499.chunk.js"
+  },
+  {
+    "revision": "a700ff875c24dcc7b8f8",
+    "url": "/static/js/1.a700ff87.chunk.js"
+  },
+  {
+    "revision": "5c286499f8e04e62f1bd",
+    "url": "/static/css/main.9e1189ad.chunk.css"
+  },
+  {
+    "revision": "f761db36f12ad8ffdfb48555a85fecb6",
+    "url": "/index.html"
+  }
+];
