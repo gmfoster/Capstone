cc924de25e8602be2622bf62b3532db1b4b2a5ac
Blake Johnson <brjohnson61@gmail.com>
2018-12-02 09:16:13 -0800

    pastebin search scrape

--- Prototyping/pastebin/pastebin.py
+++ Prototyping/pastebin/pastebin.py
@@ -3,6 +3,7 @@ import urllib.request
 import requests
 import json
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 from sys import platform
 import os
 
@@ -18,7 +19,9 @@ class Pastebin_Module():
         elif platform == "win32":
             subDir = "windows/chromedriver.exe"
         self.driverDirectory = dirPath + "/chromedriver/" + subDir
-        self.browser = webdriver.Chrome(self.driverDirectory)
+        self.chrome_options = Options()
+        #self.chrome_options.add_argument('headless')
+        self.browser = webdriver.Chrome(self.driverDirectory, options = self.chrome_options)
     
     #def search(self, searchTerm):
         #header = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.77 Safari/537.36'}
@@ -29,8 +32,38 @@ class Pastebin_Module():
         #print(search.json)
 
     def seleniumSearch(self, searchTerm):
+        pasteKeys = []
+        urls = []
+        numPages = 1
+        currentPage = 1
+
         self.browser.get("https://www.pastebin.com/search?q="+searchTerm)
-        
+        pageRoot = self.browser.find_element_by_class_name("gsc-cursor")
+        pageRootChildren = pageRoot.find_elements_by_css_selector("*")
+
+        if(len(pageRootChildren) != 0):
+            numPages = len(pageRootChildren)
+
+        print("Number of pages: " + str(numPages))
+
+        while(currentPage <= numPages):
+            elementRoots = self.browser.find_elements_by_css_selector(".gs-webResult.gs-result")
+            for element in elementRoots:
+                e = element.find_element_by_css_selector(".gs-bidi-start-align.gs-visibleUrl.gs-visibleUrl-long")
+                urls.append(str(e.text))
+
+            print("Number of urls found on this page: " + str(len(urls)))
+
+            for element in urls:
+                print(element)
+
+            if(currentPage < numPages):
+                pageRootChildren[currentPage].click()
+            currentPage = currentPage + 1
+
+        # //*[@id="___gcse_0"]/div/div/div/div[5]/div[2]/div/div/div[2]/div[11]/div/div[1]
+        # //*[@id="___gcse_0"]/div/div/div/div[5]/div[2]/div/div/div[2]/div[11]/div/div[2]
+        # //*[@id="___gcse_0"]/div/div/div/div[5]/div[2]/div/div/div[2]/div[11]/div/div[3]
 
 if __name__ == "__main__":
     paste = Pastebin_Module()
