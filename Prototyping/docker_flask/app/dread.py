78dbd61ce4c4554815785c7787d94559f2443e5b
gmfoster <gmfoster@ucsb.edu>
2019-02-12 14:22:20 -0800

    Finished dread scraper. May not work due to tor directory issues, need to figure out how to use a relative directory

---
+++ Prototyping/docker_flask/app/dread.py
@@ -0,0 +1,115 @@
+from tbselenium.tbdriver import TorBrowserDriver
+import tbselenium.common as cm
+from tbselenium.utils import launch_tbb_tor_with_stem
+from tbselenium.utils import start_xvfb, stop_xvfb
+from pyvirtualdisplay import Display
+from time import sleep
+import pyrebase
+import hashlib
+
+
+class Dread_Module():
+    def __init__(self):
+        self.search_url = 'http://dreadditevelidot.onion/search/'
+        self.tbb_dir = '/home/graham/Desktop/Capstone/Prototyping/docker_flask/app/tor-browser/'
+
+        self.config = {
+                    "apiKey": "AIzaSyCGkOiKMSxR9NRM-d1WkC2kEYOGp2d8j5k",
+                    "authDomain": "novacoast-capstone.firebaseapp.com",
+                    "databaseURL": "https://novacoast-capstone.firebaseio.com",
+                    "projectId": "novacoast-capstone",
+                    "storageBucket": "novacoast-capstone.appspot.com",
+                    "messagingSenderId": "1039131724249"
+                }
+        self.firebase = pyrebase.initialize_app(self.config)
+        self.db = self.firebase.database()
+
+    #Because for some dumb reason driver won't wait until new page is loaded after click.
+    def waitTillPageIsLoaded(self,currentPage,driver):
+            newPage = currentPage
+
+            while (currentPage == newPage):
+                sleep(1)
+                #Wait...
+                try:
+                    newPage = driver.find_element_by_tag_name('html');
+                except:
+                    pass;
+                #print "NEW",newPage
+            sleep(2)
+
+    def run(self, queries, wait_time=0):
+        xvfb_display = start_xvfb()
+
+        results = []
+
+
+        tor_process = launch_tbb_tor_with_stem(tbb_path=self.tbb_dir)
+        with TorBrowserDriver(self.tbb_dir, tor_cfg=cm.USE_STEM) as driver:
+
+        #for testing
+        #with TorBrowserDriver(tbb_dir) as driver:
+
+            driver.load_url(self.search_url)
+
+            #if (WhichIsCurrentPage(driver) == SEARCH_PAGE):
+
+
+            for q in queries:
+                nextPage = True
+                currentPage = driver.find_element_by_tag_name('html')
+
+                searchform = driver.find_element_by_class_name('inner')
+                searchform.find_element_by_name('q').send_keys(q)
+                searchform.find_element_by_class_name('standardBtn').click()
+
+                self.waitTillPageIsLoaded(currentPage,driver)
+
+                id = hashlib.md5(q.encode()).hexdigest()
+                postNum = 0
+
+                while (nextPage == True):
+                    postBoard = driver.find_element_by_class_name("postBoard")
+                    items = postBoard.find_elements_by_class_name("item")
+                    comments = postBoard.find_elements_by_class_name("item.comment")
+                    for c in comments:
+                        items.remove(c)
+                    for l in items:
+                        header = l.find_element_by_class_name("postTop")
+                        links = header.find_element_by_css_selector("a[href]")
+                        url = links.get_attribute('href')
+                        text = links.get_attribute('text')
+                        data = {"title": text, "link":url}
+                        self.db.child("dark_search").child(id).child(postNum).set(data)
+                        postNum += 1
+                    for c in comments:
+                        body = c.find_element_by_class_name("cont")
+                        title = body.find_element_by_class_name("post-title")
+                        links = title.find_element_by_css_selector("a[href]")
+                        url = links.get_attribute('href')
+                        text = links.get_attribute('text')
+                        data = {"title": text, "link": url}
+                        self.db.child("dark_search").child(id).child(postNum).set(data)
+                        postNum += 1
+                    try:
+                        pages = driver.find_element_by_class_name("pagination")
+                        pages.find_element_by_class_name("next").click()
+                        sleep(10)
+                    except:
+                        nextPage = False
+
+
+                sleep(10)
+
+
+        stop_xvfb(xvfb_display)
+        tor_process.kill()
+        return
+
+if __name__ == '__main__':
+    print("searching Dread")
+    ds = Dread_Module()
+    queries = ['netflix accounts']
+    dread_results = ds.run(queries=queries, wait_time=0)
+
+
