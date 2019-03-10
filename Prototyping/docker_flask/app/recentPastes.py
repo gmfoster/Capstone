23107433dcf9df2bfe84c871ef0d785c08dbc8af
gmfoster <gmfoster@ucsb.edu>
2019-03-10 14:42:14 -0700

    added time posted for recent pastes

--- Prototyping/docker_flask/app/recentPastes.py
+++ Prototyping/docker_flask/app/recentPastes.py
@@ -12,6 +12,7 @@ class Recent_Pastes():
     def __init__(self):
         self.url = 'https://scrape.pastebin.com/api_scraping.php?limit=25'
         self.keys = []
+        self.times = []
         self.config = {
             "apiKey": "AIzaSyCGkOiKMSxR9NRM-d1WkC2kEYOGp2d8j5k",
             "authDomain": "novacoast-capstone.firebaseapp.com",
@@ -35,33 +36,37 @@ class Recent_Pastes():
         id = hashlib.md5(keyword.encode()).hexdigest()
         scrape_data = ""
         for items in json_response:
-            i = items['scrape_url']
+            dict = {"i":items['scrape_url'],"j":items['date']}
             #p = items['key']
             #scrape_data = scrape_data + p + '\n'
             print(items['scrape_url'])
             #if (p == keyword):
             #    print("match!")
-            scrape = requests.get(i)
+            scrape = requests.get(dict["i"])
+            date = dict["j"]
+            #print(date)
             text = scrape.text
             if (text.find(keyword) != -1):
                 print("Found Word In Paste: ", items['key'])
                 print(scrape.text)
                 self.keys.append(items['key'])
+                self.times.append(date)
             time.sleep(.7)
         #print(self.keys)
         link = ''
-        for l in self.keys:
+        for l,t in zip(self.keys, self.times):
             currentDT = datetime.datetime.now()
             currentTime = currentDT.strftime("%d:%I:%M:%S")
-            data = {"link":"http://pastebin.com/" + l, "time discovered(day:hour:minute:second)":currentTime}
+            data = {"link":"http://pastebin.com/" + l, "time discovered(day:hour:minute:second)":currentTime, "time posted":t}
             link = "http://pastebin.com/" + l
             self.db.child("paste_search").child(id).child(l).set(data)
         return(len(self.keys),link)
         
 
-#if __name__ == "__main__":
-#    paste = Recent_Pastes()
-#    paste.search('TEST')
+if __name__ == "__main__":
+    paste = Recent_Pastes()
+    while(1):
+        paste.search('TEST')
 #    keyword = "5973"
 #    id = hashlib.md5(keyword.encode()).hexdigest()
 #    print(id)
